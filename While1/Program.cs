9330d1719c0d2fe82830a4364c6cd8fc8ece71e9
JaBaca67 <jabaca@uamv.edu.ni>
2026-05-04 10:30:00 -0600

    Ejercicio #1 con while

---
+++ While1/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Leer un numero entero y mostrar todos sus antesores elevados al cubo */
+using Math = System.Math;
+int numero;
+
+while (true)
+{
+    Console.Write("Ingrese un numero entero: ");
+    if (int.TryParse(Console.ReadLine()!, out numero) && numero >= 1)
+    {
+        break;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Entrada no valida, Por favor, ingrese un numero entero");
+        Console.ResetColor();
+    }
+}
+    Console.WriteLine("Los antecesores elevados al cubo son");
+
+    int contador = 1;
+    while (contador < numero)
+    {
+        double cubo = Math.Pow(contador, 3);
+        Console.WriteLine($"{contador} elevado al cubo = {cubo}");
+        contador++;
+    }
