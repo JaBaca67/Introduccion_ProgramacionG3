0270301d9f17aa4155bfee6c3c3f18f93441d59e
JaBaca67 <jabaca@uamv.edu.ni>
2026-05-06 10:31:30 -0600

    feat: add proyecto arreglo2

---
+++ arreglo1/Program.cs
@@ -0,0 +1,42 @@
+﻿/* Alamacenar 10 edad en un arrelgo, luego calcular el promedio de edad, el maximo y el minimo. */
+//crear arreglo de edades
+
+using System.ComponentModel;
+
+int[] edades = new int[10];
+
+//Pedir edades
+for (int i = 0; i < edades.Length; i++)
+{
+    try
+    {
+        Console.WriteLine("Digite la edad " + (i + 1) + ":");
+        edades[i] = int.Parse(Console.ReadLine()!);
+    }
+    catch (FormatException)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Entrada inválida. Por favor, ingrese un número entero.");
+        i--; // Reintentar la misma posición
+        Console.ResetColor();
+    }
+}
+
+//Calcular promedio, maximo y minimo
+int suma = 0, maximo = edades[0], minimo = edades[0];
+
+foreach(int edad in edades)
+{
+    suma += edad;
+    if (edad > maximo)
+        maximo = edad;
+    if (edad < minimo)
+        minimo = edad;
+}
+
+double promedio = (double)suma / edades.Length;
+
+//Mostrar resultados
+Console.WriteLine("El promedio de edad es: " + promedio);
+Console.WriteLine("La edad máxima es: " + maximo);
+Console.WriteLine("La edad mínima es: " + minimo);
---
+++ arreglo2/Program.cs
@@ -0,0 +1,69 @@
+﻿// Crea un arrelgo de notas de 15 estudiantes, luego calcula el promedio de las notas, y los 3 primeros lugaes. No se puede ingresar notas menores a 0 o mayores a 100.
+
+int[] notas = new int[15];
+
+int suma = 0, countErrors = 0;
+double promedio = 0;
+
+for (int i = 0; i < notas.Length; i++)
+{
+    if (countErrors >= 3)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("Te hace falta matria gris. Respia y oxigena y vuelve a nacer. Adios. imbecil, voy a destruir la computadora.");
+        Console.ResetColor();
+    }
+    try
+    {
+        Console.WriteLine($"Ingrese la nota del estudiante {i + 1}:");
+        int nota = int.Parse(Console.ReadLine()!);
+
+        if (nota < 0 || nota > 100)
+        {
+            throw new ArgumentOutOfRangeException("La nota debe ser entre 0 y 100.");
+        }
+        notas[i] = nota;
+    }
+
+    catch (FormatException ex)
+    {
+        Console.WriteLine("Error: " + ex.Message);
+        i--;
+        countErrors++;
+    }
+
+    catch (ArgumentOutOfRangeException ex)
+    {
+        Console.WriteLine("Error: " + ex.Message);
+        i--;
+        countErrors++;
+    }
+
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error: " + ex.Message);
+        i--;
+        countErrors++;
+    }
+
+
+}
+
+foreach (int nota in notas)
+{
+    suma += nota;
+}
+promedio = suma / notas.Length;
+Console.WriteLine($"La suma de las notas es: {suma}");
+Console.WriteLine($"El numero de errores es: {countErrors}");
+Console.WriteLine($"El promedio de las notas es: {promedio}");
+
+Array.Sort(notas);
+Array.Reverse(notas);
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Cuadro de Honor");
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine(notas[i]);
+}
